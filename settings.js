348dc1e925a67089fea8ac13166664ec5e7be3d6
mclarenxcode <mclarenxcode@gmail.com>
2025-12-05 15:30:51 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝑴𝑪𝑳𝑨𝑹𝑬𝑵 𝑿𝑰𝑹',
+  author: '‎',
+  botName: "𝑴𝑪𝑳𝑨𝑹𝑬𝑵 𝑿𝑰𝑹",
+  botOwner: 'Mclaren', // Your name
+  ownerNumber: '‎‪2347020229806', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.5",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
