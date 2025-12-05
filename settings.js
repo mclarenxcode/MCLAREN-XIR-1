184f7229741bb313de1b459e04caf3a044dc4143
mclarenxcode <mclarenxcode@gmail.com>
2025-12-05 15:45:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.5",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/mclarenxcode/MCLAREN-XIR-1",
 };
 
 module.exports = settings;
